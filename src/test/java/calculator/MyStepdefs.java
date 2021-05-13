0f3a17a7e60bfd14f6dbd8aac548ebd3566f51af
narges <jatages@gmail.com>
2021-05-13 18:22:06 +0430

    Refactor tests

--- src/test/java/calculator/MyStepdefs.java
+++ src/test/java/calculator/MyStepdefs.java
@@ -5,19 +5,15 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class MyStepdefs {
-    @Given("^Two input values, (\\d+) and (\\d+)$")
-    public void twoInputValuesAnd(int arg0, int arg1) {
-    }
-
-    @When("^I divide the two values$")
-    public void iDivideTheTwoValues() {
+    @When("^I operate the two values$")
+    public void iOperateTheTwoValues() {
     }
 
     @Then("^I expect the result (\\d+)$")
     public void iExpectTheResult(int arg0) {
     }
 
-    @When("^I Increase the first number to the power of second number$")
-    public void iIncreaseTheFirstNumberToThePowerOfSecondNumber() {
+    @Given("^two input values, (\\d+), (\\d+) and an opt '\\^'$")
+    public void twoInputValuesAndAnOpt(int arg0, int arg1) {
     }
 }
--- src/test/resources/features/calculator.feature
+++ src/test/resources/features/calculator.feature
@@ -1,12 +1,7 @@
 @tag
 Feature: Calculator
 
-  Scenario: divide two numbers
-    Given Two input values, 6 and 2
-    When I divide the two values
-    Then I expect the result 3
-
-  Scenario: power two numbers
-    Given Two input values, 2 and 5
-    When I Increase the first number to the power of second number
-    Then I expect the result 32
+  Scenario: operate two numbers
+    Given two input values, 2, 6 and an opt '^'
+    When I operate the two values
+    Then I expect the result 64
